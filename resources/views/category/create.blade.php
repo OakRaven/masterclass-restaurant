d982608fd0f9833905a7c6a67b192431669453d9
Mike Hatfield <oakraven13@gmail.com>
2020-12-13 18:59:28 -0400

    Add inline validation to category add/edit

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -2,43 +2,48 @@
 
 @section('content')
 <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">
-          @if (isset($category))
-          Update Category
-          @else
-          Create Category
-          @endif
-        </div>
-        <div class="card-body">
-          @include('partials.errors')
-
-          <form action="{{ isset($category) ? route('category.update', $category) : route('category.store') }}"
-            method="POST">
-            @csrf
-            @if (isset($category))
-            @method('PUT')
-            @endif
-            <div class="form-group">
-              <label for="name">Name</label>
-              <input type="text" name="name" id="name" class="form-control"
-                value="{{ old('name', isset($category) ? $category->name : '') }}">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                    @if (isset($category))
+                    Update Category
+                    @else
+                    Create Category
+                    @endif
+                </div>
+                <div class="card-body">
+                    <form
+                        action="{{ isset($category) ? route('category.update', $category) : route('category.store') }}"
+                        method="POST">
+                        @csrf
+                        @if (isset($category))
+                        @method('PUT')
+                        @endif
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" id="name"
+                                class="form-control @error('name') is-invalid @enderror"
+                                value="{{ old('name', isset($category) ? $category->name : '') }}">
+                            @error('name')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-outline-primary">
+                                @if (isset($category))
+                                Update Category
+                                @else
+                                Create Category
+                                @endif
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-            <div class="form-group">
-              <button type="submit" class="btn btn-outline-primary">
-                @if (isset($category))
-                Update Category
-                @else
-                Create Category
-                @endif
-              </button>
-            </div>
-          </form>
         </div>
-      </div>
     </div>
-  </div>
 </div>
 @endsection
